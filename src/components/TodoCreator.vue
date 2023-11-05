ca83d6350c40a069f9be6531e62071df883e225a
szvitek <gy.szvitek@gmail.com>
2023-11-05 13:36:28 +0000

    init(TodoButton): testing slots

--- src/components/TodoCreator.vue
+++ src/components/TodoCreator.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { defineEmits, reactive } from 'vue'
+import TodoButton from './TodoButton.vue'
 
 const emit = defineEmits(['create-todo'])
 
@@ -26,7 +27,7 @@ const createTodo = () => {
 <template>
   <div class="input-wrap" :class="{ 'input-err': todoState.invalid }">
     <input type="text" v-model="todoState.todo" />
-    <button @click="createTodo()">Create</button>
+    <TodoButton @click="createTodo" />
   </div>
   <p v-show="todoState.invalid" class="err-msg">
     {{ todoState.errMsg }}
@@ -57,11 +58,6 @@ const createTodo = () => {
     &:focus {
       outline: none;
     }
-  }
-
-  button {
-    padding: 8px 16px;
-    border: none;
   }
 }
 
