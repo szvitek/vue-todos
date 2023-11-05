7cfb12ddabf7552ae7c4dbaab0ed3aab8fa1d411
szvitek <gy.szvitek@gmail.com>
2023-11-05 16:37:45 +0000

    feat(About): about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Vue Todos</h1>
+    <div>Vue Todos is a CRUD Todo tracking application created with Vue 3 composation API</div>
+    <div>
+      Track your daily tasks's using this todo tracker with abilities to save, complete, edit &
+      delete todos.
+    </div>
   </div>
 </template>
 
@@ -9,7 +14,8 @@
   .about {
     min-height: 100vh;
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    gap: 10px;
   }
 }
 </style>
